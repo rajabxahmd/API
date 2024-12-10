fe12b975177151329e5802166e82a2eadf6a1087
Ahmed Rajab <rajabxamd.tva@gmail.com>
2024-12-10 13:16:29 +0530

    instead of hardcoding the url, created an external env file

--- fetchingproject.js
+++ fetchingproject.js
@@ -1,5 +1,5 @@
 const cardDetails= document.getElementById("details-card")
-fetch(`https://jsonplaceholder.typicode.com/users/${id=1}`)
+fetch(`${BASE_URL}/${id=1}`)
     .then(response => response.json())
     .then(data => cardDetails.textContent=data)
     .catch(error => console.log(error));

429ebdd57085529ee4cbc07a670084df955fe4f1
Ahmed Rajab <rajabxamd.tva@gmail.com>
2024-12-10 19:31:04 +0530

    updated the code, used a recursive function to iterate through nested objects and changed the env file.

--- fetchingproject.js
+++ fetchingproject.js
@@ -1,5 +1,23 @@
-const cardDetails= document.getElementById("details-card")
-fetch(`${BASE_URL}/${id=1}`)
+const cardDetails= document.getElementById("details-card");
+const id=1;
+fetch(`${BASE_URL}/${id}`)
     .then(response => response.json())
-    .then(data => cardDetails.textContent=data)
-    .catch(error => console.log(error));
+    .then(data => {
+        const obj = data ;
+        function objectTraversing(obj){
+            for (const key in obj){
+                if(typeof obj[key]==="object" && obj[key]!==null){
+                    cardDetails.innerHTML+=`<br>${key}<br>`;
+                    objectTraversing(obj[key])
+                }
+                else{
+                    cardDetails.innerHTML+=`${key} : ${obj[key]}<br>`;
+                }
+            }
+        }
+        objectTraversing(obj);
+    })
+    .catch(error => {
+        console.error(error)
+        cardDetails.innerHTML="error fetching data"
+    });
